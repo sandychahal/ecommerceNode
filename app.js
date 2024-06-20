2f4191d4b1e10b2b9a6b144e4c18a763c0fafbe5
Deepank Khera <deepankkhera.dk201314@gmail.com>
2024-06-20 21:40:06 +0530

    updated app.js
    removed excess code lines

--- app.js
+++ app.js
@@ -1,38 +1,23 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import mysql from 'mysql2';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import { dbConnect } from './config/dbConfig.js';
+
 dotenv.config();
+
 const app = express();
-const port = process.env.PORT || 3000;
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
 
+app.use(express.json());
 
-const pubdir = path.join(__dirname,'./views/public');
-app.use(express.static(pubdir));
+// Connect to the database
+dbConnect();
 
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/user', userRoutes);
 
-db.connect((error)=>{
-    if(error){
-        console.log("Error connecting to the database: ",error.stack);
-        return;
-    }
-    console.log('Connected to database...');
-});
-
-
-app.get('/', (req,res) => {
-    res.render('/views/public/index');
-});
-
-app.listen(port, ()=>{
-    console.log(`Server started on Port ${port}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
