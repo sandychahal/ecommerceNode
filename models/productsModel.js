20ba603a348ea77616c2ddaba8ccdd4117d17ae4
Rohit-Singh-0 <rohitsingh3640@gmail.com>
2024-06-24 17:04:22 +0530

    added products routes
    Added product api's for getting product by product id and one for adding the product in the database

--- models/productsModel.js
+++ models/productsModel.js
@@ -18,7 +18,34 @@ const getProductByCategory = (id, callback) => {
   });
 };
 
+const getUniqueProduct =(id,callback)=>{
+  db.query('SELECT * FROM products WHERE p_id = ?',[id],(err,results)=>{
+    if (err) {
+      return callback(err);
+    }
+    callback(null, results);
+  })
+}
+
+const checkProductExists =(name,callback)=>{
+  db.query('SELECT * FROM products WHERE name = ?',[name],(err,results)=>{
+    if (err) {
+      return callback(err);
+    }
+    callback(null, results);
+  })
+}
+
+const createProduct = (name, cat_id, desc, pfp, cp,sp,mrp,avg_ratings,avg_reviews,created_by,updated_by,callback) => {
+  const query =
+    'INSERT INTO products (name, cat_id, `desc`, pfp, cp,sp,mrp,avg_ratings,avg_reviews,created_by,updated_by) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
+  db.query(query, [name, cat_id, desc, pfp, cp,sp,mrp,avg_ratings,avg_reviews,created_by,updated_by], callback)
+}
+
 module.exports = {
   getAllProducts,
   getProductByCategory,
+  getUniqueProduct,
+  checkProductExists,
+  createProduct
 }
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const { all, filter } = require('../controllers/productsController')
+const { all, filter, filterProduct, addProduct } = require('../controllers/productsController')
 
 router.get('/all', all)
 router.get('/filter/:id', filter)
+router.get('/filterProduct/:id',filterProduct)
+router.post('/add',addProduct)
 
 module.exports = router
