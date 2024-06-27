3d46b2f0360f56587332e26688ce037beeef289e
Anurag <anuragmount9868@gmail.com>
2024-06-27 12:48:41 +0530

    Added verification of admin before allowing to add new product

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const { all, filter, add } = require('../controllers/productsController')
-const authenticateToken = require('../middlewares/authenticateToken');
+// const authenticateToken = require('../middlewares/authenticateToken');
+const verifyAdmin = require('../middlewares/verifyAdmin');
 
 router.get('/all', all)
 router.get('/filter/:id', filter)
-router.post('/add',authenticateToken, add)
+router.post('/add',verifyAdmin, add)
 
 module.exports = router
