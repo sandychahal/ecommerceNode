1f847db009db043a0e66a34d2a084a40510017e8
Rohit-Singh-0 <rohitsingh3640@gmail.com>
2024-06-29 16:51:24 +0530

    Added API's for categories table

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const userRoutes = require('./routes/userRoutes')
 const managementRoutes = require('./routes/managementRoutes')
 const productsRoutes = require('./routes/productRoutes')
+const categoryRoutes = require('./routes/categoryRoutes')
 
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -14,6 +15,8 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/users', userRoutes)
 app.use('/management', managementRoutes)
 app.use('/products', productsRoutes)
+app.use('/category', categoryRoutes)
+app.use('/attributes', categoryRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
